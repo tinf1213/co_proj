33e4b155d1d2638fa606361bd11216651e81fea3
t1nf <ericding1213@gmail.com>
2024-06-20 15:59:40 +0000

    Q5 adjust using mul

--- demo/WTWB/back/script.sh
+++ demo/WTWB/back/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 gem5_path=~/co_proj/gem5
-result_path=~/co_proj/demo/WTWB/back
+result_path=~/co_proj/demo/WTWB/through
 
 benchmark_mul=~/co_proj/benchmark/multiply
 benchmark_qsort=~/co_proj/benchmark/quick_small
 
-arg='--cpu-type=TimingSimpleCPU --caches --l2cache --l3cache --mem-type=NVMainMemory --nvmain-config=/home/t1nf/co_proj/NVmain/Config/PCM_ISSCC_2012_4GB.config --l1i_size=32kB --l1d_size=32kB --l2_size=128kB --l3_size=128kB --l3_assoc=1'
+arg='--cpu-type=TimingSimpleCPU --caches --l2cache --l3cache --mem-type=NVMainMemory --nvmain-config=/home/t1nf/co_proj/NVmain/Config/PCM_ISSCC_2012_4GB.config --l1i_size=32kB --l1d_size=32kB --l2_size=128kB --l3_size=128kB --l3_assoc=4'
 
-${gem5_path}/build/X86/gem5.opt ${gem5_path}/configs/example/se.py -c $benchmark_qsort $arg > ${result_path}/result 
+${gem5_path}/build/X86/gem5.opt ${gem5_path}/configs/example/se.py -c $benchmark_mul $arg > ${result_path}/result 
